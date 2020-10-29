c28806ce69c3a93c79a45af0be0a708fcc43ac18
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-29 16:06:13 +0530

    [Deepanjan]Add .Ability to Calculate total fare.

---
+++ src/main/java/org/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,14 @@
+package org.bridgelabz.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+
+	public double calculateFare(double distance, int time) {
+		// TODO Auto-generated method stub
+		return distance * MINIMUM_COST_PER_KILOMETER+ time * COST_PER_TIME;
+				
+	}
+
+}
