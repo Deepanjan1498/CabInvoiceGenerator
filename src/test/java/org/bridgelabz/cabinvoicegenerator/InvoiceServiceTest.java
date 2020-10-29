74ecd32a6f638875ca11ef6b349e18f771510873
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-29 16:17:10 +0530

    [Deepanjan]Add .Ability to Calculate total fare for the journey.

--- src/test/java/org/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/org/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -11,7 +11,15 @@ public class InvoiceServiceTest {
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance,time);
-		Assert.assertEquals(25.0, fare,0.0);
-		
+		Assert.assertEquals(25.0, fare,0.0);		
+	}
+	
+	@Test
+	public void givenLessDistanceOrTime_SHouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time =1;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(5,fare,0.0);
 	}
 }
