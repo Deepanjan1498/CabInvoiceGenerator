a1aa77bcda55dd4bd083cf9745a57d2fa886e1ac
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-29 16:40:59 +0530

    [Deepanjan]Add .Ability to add multiple rides and its testcase is added.

---
+++ src/main/java/org/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,12 @@
+package org.bridgelabz.cabinvoicegenerator;
+
+public class Ride {
+	public double distance;
+	public final int time;
+
+	public Ride(double distance,int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
